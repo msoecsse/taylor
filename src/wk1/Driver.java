cdba8390949bd515876c86acaca3130867a9470f
Chris Taylor <gitlab@taylor.bz>
2022-03-08 07:04:05 -0600

    Week 1 Lec 1

---
+++ src/wk1/Driver.java
@@ -0,0 +1,13 @@
+package wk1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Driver {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("two");
+        collection.add("to");
+        collection.add("too");
+    }
+}
